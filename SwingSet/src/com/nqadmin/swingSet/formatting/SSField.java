2fdaf1b35b615125b60d1b27b253c97d69d85d97
dags <>
2005-05-26 22:17:38 +0000

    initial release

---
+++ SwingSet/src/com/nqadmin/swingSet/formatting/SSField.java
@@ -0,0 +1,12 @@
+package com.nqadmin.swingSet.formatting;
+
+public interface SSField {
+
+    /**
+    *
+    *
+    * This method must set the field to an initial state: empty or with default value.
+    **/
+    public void cleanField();
+
+}
