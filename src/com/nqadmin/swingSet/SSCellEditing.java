2174f9f559c492d439ad8f4d20f50a3126628cba
Brian E. Pangburn <yoda@greatmindsworking.com>
2019-03-06 19:09:16 -0600

    Default methods for SSCellEditing and deprecated SSCellEditingAdapter

--- src/com/nqadmin/swingSet/SSCellEditing.java
+++ src/com/nqadmin/swingSet/SSCellEditing.java
@@ -37,69 +37,75 @@
 
 package com.nqadmin.swingSet;
 
+import java.io.Serializable;
+
 /**
  * SSCellEditing.java
  * 
  * SwingSet - Open Toolkit For Making Swing Controls Database-Aware
  * 
- * The SSCellEditing interface specifies the methods the SSTableModel will use to
- * determine whether or not a given cell can be edited or if a user-specified
+ * The SSCellEditing interface specifies the methods the SSTableModel will use
+ * to determine whether or not a given cell can be edited or if a user-specified
  * value for a cell is valid or invalid.
  */
-public interface SSCellEditing {
+public interface SSCellEditing extends Serializable {
 
-    /**
-     * Returns true if the cell at row _row and at column _column is editable else
-     * false.
-     *
-     * SSTableModel first looks in to uneditable columns, if the column is not
-     * in the uneditable columns list then this function is called (If SSCellEditing
-     * is implemented).
-     *
-     * @param _row    the row to which the cell belongs.
-     * @param _column    the column to which the cell belongs.
-     *
-     * @return returns true is the cell is editable else false.
-     */
-    public boolean isCellEditable(int _row, int _column);
+	/**
+	 * unique serial id
+	 */
+	static final long serialVersionUID = -8081589343308592606L;
 
-    /**
-     * This function is called when ever a update to a cell is done but before
-     * the value is updated in the database.<BR>
-     * If the function returns false the update is cancelled, if it returns true
-     * the value will be updated in the database.<BR>
-     *
-     * @param _row    the row in which update is taking place.
-     * @param _column    the column at which update is taking place.
-     * @param _oldValue    the present value in the cell being edited.
-     * @param _newValue    the new value entered in the cell being edited.
-     *
-     * @return returns true if update should be made else false.
-     */
-    public boolean cellUpdateRequested(int _row, int _column, Object _oldValue, Object _newValue);
+	/**
+	 * Returns true if the cell at row _row and at column _column is editable else
+	 * false.
+	 *
+	 * SSTableModel first looks in to uneditable columns, if the column is not in
+	 * the uneditable columns list then this function is called (If SSCellEditing is
+	 * implemented).
+	 *
+	 * @param _row    the row to which the cell belongs.
+	 * @param _column the column to which the cell belongs.
+	 *
+	 * @return returns true is the cell is editable else false.
+	 */
+	default boolean isCellEditable(int _row, int _column) {
+		return true;
+	}
+
+	/**
+	 * This function is called when ever a update to a cell is done but before the
+	 * value is updated in the database.<BR>
+	 * If the function returns false the update is cancelled, if it returns true the
+	 * value will be updated in the database.<BR>
+	 *
+	 * @param _row      the row in which update is taking place.
+	 * @param _column   the column at which update is taking place.
+	 * @param _oldValue the present value in the cell being edited.
+	 * @param _newValue the new value entered in the cell being edited.
+	 *
+	 * @return returns true if update should be made else false.
+	 */
+	default boolean cellUpdateRequested(int _row, int _column, Object _oldValue, Object _newValue) {
+		return true;
+	}
 
 } // end public interface SSCellEditing {
 
-
-
 /*
- * $Log$
- * Revision 1.6  2005/02/04 22:48:52  yoda2
- * API cleanup & updated Copyright info.
+ * $Log$ Revision 1.6 2005/02/04 22:48:52 yoda2 API cleanup & updated Copyright
+ * info.
  *
- * Revision 1.5  2004/11/11 14:45:33  yoda2
- * Using TextPad, converted all tabs to "soft" tabs comprised of four actual spaces.
+ * Revision 1.5 2004/11/11 14:45:33 yoda2 Using TextPad, converted all tabs to
+ * "soft" tabs comprised of four actual spaces.
  *
- * Revision 1.4  2004/08/10 22:06:59  yoda2
- * Added/edited JavaDoc, made code layout more uniform across classes, made various small coding improvements suggested by PMD.
+ * Revision 1.4 2004/08/10 22:06:59 yoda2 Added/edited JavaDoc, made code layout
+ * more uniform across classes, made various small coding improvements suggested
+ * by PMD.
  *
- * Revision 1.3  2004/03/08 16:43:37  prasanth
- * Updated copyright year.
+ * Revision 1.3 2004/03/08 16:43:37 prasanth Updated copyright year.
  *
- * Revision 1.2  2003/12/18 20:12:40  prasanth
- * Update class description.
+ * Revision 1.2 2003/12/18 20:12:40 prasanth Update class description.
  *
- * Revision 1.1  2003/12/16 18:02:47  prasanth
- * Initial version.
+ * Revision 1.1 2003/12/16 18:02:47 prasanth Initial version.
  *
  */
--- src/com/nqadmin/swingSet/SSCellEditingAdapter.java
+++ src/com/nqadmin/swingSet/SSCellEditingAdapter.java
@@ -37,71 +37,67 @@
 
 package com.nqadmin.swingSet;
 
-import java.io.Serializable;
-
 /**
  * SSCellEditingAdapter.java
  * 
  * SwingSet - Open Toolkit For Making Swing Controls Database-Aware
  * 
- * This abstract adapter class is provided as a convenience for creating
- * custom SSCellEditing objects. Extend this class to create a SSCellEditing
+ * This abstract adapter class is provided as a convenience for creating custom
+ * SSCellEditing objects. Extend this class to create a SSCellEditing
  * implementation.
  *
- * SSCellEditingAdapter defines empty functions so that the programmer can define
- * only the functions desired. Both isCellEditable() and cellUpdateRequested()
- * always return true.
+ * SSCellEditingAdapter defines empty functions so that the programmer can
+ * define only the functions desired. Both isCellEditable() and
+ * cellUpdateRequested() always return true.
+ * 
+ * @deprecated Starting in 2.3.0+ use {@link SSCellEditing} instead.
+ * 
  */
-public abstract class SSCellEditingAdapter implements SSCellEditing, Serializable {
+@Deprecated
+public abstract class SSCellEditingAdapter implements SSCellEditing {
 
-    /**
+	/**
 	 * unique serial id
 	 */
 	private static final long serialVersionUID = -8081589658308592606L;
 
 	/**
-     * This empty implementation always returns true.
-     * For description about the function look in SSCellEditing class.
-     */
-    @Override
+	 * This empty implementation always returns true. For description about the
+	 * function look in SSCellEditing class.
+	 */
+	@Override
 	public boolean isCellEditable(int _row, int _column) {
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * This empty implementation always returns true.
-     * For description about the function look in SSCellEditing class.
-     */
-    @Override
-	public boolean cellUpdateRequested(int _row, int _column, Object _oldValue, Object _newValue){
-        return true;
-    }
+	/**
+	 * This empty implementation always returns true. For description about the
+	 * function look in SSCellEditing class.
+	 */
+	@Override
+	public boolean cellUpdateRequested(int _row, int _column, Object _oldValue, Object _newValue) {
+		return true;
+	}
 
 } // end public abstract class SSCellEditingAdapter
 
-
-
 /*
- * $Log$
- * Revision 1.7  2005/02/04 22:48:52  yoda2
- * API cleanup & updated Copyright info.
+ * $Log$ Revision 1.7 2005/02/04 22:48:52 yoda2 API cleanup & updated Copyright
+ * info.
  *
- * Revision 1.6  2004/11/11 14:45:48  yoda2
- * Using TextPad, converted all tabs to "soft" tabs comprised of four actual spaces.
+ * Revision 1.6 2004/11/11 14:45:48 yoda2 Using TextPad, converted all tabs to
+ * "soft" tabs comprised of four actual spaces.
  *
- * Revision 1.5  2004/08/10 22:06:58  yoda2
- * Added/edited JavaDoc, made code layout more uniform across classes, made various small coding improvements suggested by PMD.
+ * Revision 1.5 2004/08/10 22:06:58 yoda2 Added/edited JavaDoc, made code layout
+ * more uniform across classes, made various small coding improvements suggested
+ * by PMD.
  *
- * Revision 1.4  2004/08/02 14:29:46  prasanth
- * implements serializable.
+ * Revision 1.4 2004/08/02 14:29:46 prasanth implements serializable.
  *
- * Revision 1.3  2004/03/08 16:43:37  prasanth
- * Updated copy right year.
+ * Revision 1.3 2004/03/08 16:43:37 prasanth Updated copy right year.
  *
- * Revision 1.2  2003/12/18 20:13:00  prasanth
- * Update class description.
+ * Revision 1.2 2003/12/18 20:13:00 prasanth Update class description.
  *
- * Revision 1.1  2003/12/16 18:02:47  prasanth
- * Initial version.
+ * Revision 1.1 2003/12/16 18:02:47 prasanth Initial version.
  *
  */
