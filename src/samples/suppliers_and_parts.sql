84ca1f7f9d2441fa2fa6cf8c78b110f7569acd05
Brian E. Pangburn <yoda@greatmindsworking.com>
2019-10-14 20:28:48 -0500

    Upgrade to the latest H2. SQL fixes needed for sample database.

--- src/samples/suppliers_and_parts.sql
+++ src/samples/suppliers_and_parts.sql
@@ -38,61 +38,85 @@
 
 /* This SQL script is used to generate the sample suppliers_and_parts database
    for the SwingSet sample programs */
-   
+
+/* supplier_data */
 DROP TABLE IF EXISTS supplier_data;
+DROP SEQUENCE IF EXISTS supplier_data_seq;
+
+CREATE SEQUENCE IF NOT EXISTS supplier_data_seq;
 CREATE TABLE supplier_data 
 ( 
-supplier_id INT AUTO_INCREMENT, 
-supplier_name VARCHAR(50), 
-status SMALLINT, 
-city VARCHAR(50), 
-PRIMARY KEY (supplier_id) 
+    supplier_id INTEGER DEFAULT nextval('supplier_data_seq') NOT NULL PRIMARY KEY,
+    supplier_name VARCHAR(50), 
+    status SMALLINT, 
+    city VARCHAR(50)
 );
-insert into supplier_data values (2,'Jones',  10, 'Paris');
-insert into supplier_data values (5,'Adams', 30, 'Athens');
-insert into supplier_data values (4,'Clark', 20, 'London');
-insert into supplier_data values (3,'Blake', 30, 'Paris');
-insert into supplier_data values (1,'Smith',20,'London');
-select * from supplier_data;
+
+INSERT INTO supplier_data VALUES (2,'Jones',  10, 'Paris');
+INSERT INTO supplier_data VALUES (5,'Adams', 30, 'Athens');
+INSERT INTO supplier_data VALUES (4,'Clark', 20, 'London');
+INSERT INTO supplier_data VALUES (3,'Blake', 30, 'Paris');
+INSERT INTO supplier_data VALUES (1,'Smith',20,'London');
+
+ALTER SEQUENCE supplier_data_seq RESTART WITH 1000;
+
+SELECT * from supplier_data;
+
+
+/* part_data */
 DROP TABLE IF EXISTS part_data;
+DROP SEQUENCE IF EXISTS part_data_seq;
+
+CREATE SEQUENCE IF NOT EXISTS part_data_seq;
 CREATE TABLE part_data 
 ( 
-part_id INT AUTO_INCREMENT, 
-part_name VARCHAR(50), 
-color_code SMALLINT, 
-weight SMALLINT, 
-city VARCHAR(50), 
-PRIMARY KEY (part_id) 
+    part_id INTEGER DEFAULT nextval('part_data_seq') NOT NULL PRIMARY KEY,
+    part_name VARCHAR(50), 
+    color_code SMALLINT, 
+    weight SMALLINT, 
+    city VARCHAR(50)
 );
-insert into part_data values (6, 'Cog', 0, 19, 'London');
-insert into part_data values (5, 'Cam', 2, 12, 'Paris',);
-insert into part_data values (4, 'Screw', 0, 14, 'London');
-insert into part_data values (3, 'Screw', 2, 17, 'Rome',);
-insert into part_data values (2, 'Bolt', 1, 17, 'Paris');
-insert into part_data values (1, 'Nut', 0, 12, 'London');
+
+INSERT INTO part_data VALUES (6, 'Cog', 0, 19, 'London');
+INSERT INTO part_data VALUES (5, 'Cam', 2, 12, 'Paris');
+INSERT INTO part_data VALUES (4, 'Screw', 0, 14, 'London');
+INSERT INTO part_data VALUES (3, 'Screw', 2, 17, 'Rome');
+INSERT INTO part_data VALUES (2, 'Bolt', 1, 17, 'Paris');
+INSERT INTO part_data VALUES (1, 'Nut', 0, 12, 'London');
+
+ALTER SEQUENCE part_data_seq RESTART WITH 1000;
+
 SELECT * FROM part_data;
+
+/* supplier_part_data */
 DROP TABLE IF EXISTS supplier_part_data;
+DROP SEQUENCE IF EXISTS supplier_part_data_seq;
+
+CREATE SEQUENCE IF NOT EXISTS supplier_part_data_seq;
 CREATE TABLE supplier_part_data 
 ( 
-supplier_part_id INT AUTO_INCREMENT, 
-supplier_id INT, 
-part_id INT, 
-quantity SMALLINT, 
-ship_date DATE,
-PRIMARY KEY (supplier_part_id), 
-FOREIGN KEY (supplier_id) REFERENCES supplier_data(supplier_id), 
-FOREIGN KEY (part_id) REFERENCES part_data(part_id), 
+    supplier_part_id INTEGER DEFAULT nextval('supplier_part_data_seq') NOT NULL PRIMARY KEY,
+    supplier_id INT, 
+    part_id INT, 
+    quantity SMALLINT, 
+    ship_date DATE,
+    FOREIGN KEY (supplier_id) REFERENCES supplier_data(supplier_id), 
+    FOREIGN KEY (part_id) REFERENCES part_data(part_id)
 );
-Insert into supplier_part_data values (8, 3, 2, 700, '2003-10-15');
-Insert into supplier_part_data values (7, 2, 1, 400, '2004-09-15');
-Insert into supplier_part_data values (4, 4, 6, 700, '2004-03-15');
-Insert into supplier_part_data values (5, 5, 5, 100, '2004-03-25');
-Insert into supplier_part_data values (1, 1, 4, 100, '2003-10-20');
-Insert into supplier_part_data values (3, 3, 3, 400, '2003-01-05');
-Insert into supplier_part_data values (2, 2, 2, 200, '2003-01-01');
-Insert into supplier_part_data values (6, 1, 1, 700, '2003-12-12');
-Insert into supplier_part_data values (9, 4, 3, 700, '2003-12-12');
-Insert into supplier_part_data values (12, 2, 5, 400, '2003-12-12');
-Insert into supplier_part_data values (11, 1, 4, 300, '2003-12-12');
-Insert into supplier_part_data values (10, 5, 2, 200, '2003-12-12');
+
+INSERT INTO supplier_part_data VALUES (8, 3, 2, 700, '2003-10-15');
+INSERT INTO supplier_part_data VALUES (7, 2, 1, 400, '2004-09-15');
+INSERT INTO supplier_part_data VALUES (4, 4, 6, 700, '2004-03-15');
+INSERT INTO supplier_part_data VALUES (5, 5, 5, 100, '2004-03-25');
+INSERT INTO supplier_part_data VALUES (1, 1, 4, 100, '2003-10-20');
+INSERT INTO supplier_part_data VALUES (3, 3, 3, 400, '2003-01-05');
+INSERT INTO supplier_part_data VALUES (2, 2, 2, 200, '2003-01-01');
+INSERT INTO supplier_part_data VALUES (6, 1, 1, 700, '2003-12-12');
+INSERT INTO supplier_part_data VALUES (9, 4, 3, 700, '2003-12-12');
+INSERT INTO supplier_part_data VALUES (12, 2, 5, 400, '2003-12-12');
+INSERT INTO supplier_part_data VALUES (11, 1, 4, 300, '2003-12-12');
+INSERT INTO supplier_part_data VALUES (10, 5, 2, 200, '2003-12-12');
+
+ALTER SEQUENCE supplier_part_data_seq RESTART WITH 1000;
+
 SELECT * FROM supplier_part_data;
