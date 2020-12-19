e877c58d54abb413596262c39ce23436bf78a7dc
Brian E. Pangburn <swingset@pangburngroup.com>
2020-12-19 12:35:17 -0600

    Allow tabnabb(pp)ing.

--- swingset/src/main/java/com/nqadmin/swingset/models/GlazedListsOptionMappingInfo.java
+++ swingset/src/main/java/com/nqadmin/swingset/models/GlazedListsOptionMappingInfo.java
@@ -51,8 +51,8 @@ import ca.odell.glazedlists.EventList;
  * @param <O> option type; option provides display string
  * @param <O2>  option2 type; if present, supplementary display string
  * 
- * @see <a href="https://javadoc.io/doc/com.glazedlists/glazedlists/latest/ca/odell/glazedlists/swing/AutoCompleteSupport.html" target="_blank" rel="noopener noreferrer">GlazedLists AutoCompletion javadoc</a>
- * @see <a href="https://publicobject.com/glazedlistsdeveloper/screencasts/autocompletesupport/AutoCompleteSupport.wmv" target="_blank" rel="noopener noreferrer">GlazedLists AutoCompletion Video</a>
+ * @see <a href="https://javadoc.io/doc/com.glazedlists/glazedlists/latest/ca/odell/glazedlists/swing/AutoCompleteSupport.html" target="_blank">GlazedLists AutoCompletion javadoc</a>
+ * @see <a href="https://publicobject.com/glazedlistsdeveloper/screencasts/autocompletesupport/AutoCompleteSupport.wmv" target="_blank">GlazedLists AutoCompletion Video</a>
  * 
  * @since 4.0.0
  */
