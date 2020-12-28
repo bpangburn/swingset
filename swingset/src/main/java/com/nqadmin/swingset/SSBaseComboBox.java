2518ce9824c3401bbe86b74d9ff09f6fea444661
Brian Pangburn <swingset@pangburngroup.com>
2020-12-28 15:06:03 -0600

    Removed Focus Listener imports.

--- swingset/src/main/java/com/nqadmin/swingset/SSBaseComboBox.java
+++ swingset/src/main/java/com/nqadmin/swingset/SSBaseComboBox.java
@@ -42,8 +42,6 @@ package com.nqadmin.swingset;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.Serializable;
